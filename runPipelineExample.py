a3aa51c69c670da4ea8dab2e45d93526132ffa01
JorisDeRidder <joris.deridder@kuleuven.be>
2022-11-22 14:14:18 +0100

    Adapted runPipelineExample.py to the new set of simulations of MARVELsim.

--- runPipelineExample.py
+++ runPipelineExample.py
@@ -1,6 +1,7 @@
 # This is an example script on how to run the different MARVEL pipeline components
 # on a given set of data input files.
 
+import os
 from database import DatabaseFromLocalFile
 from pipeline import MasterBias, MasterFlat, CalibratedScienceFrames 
 from orderMaskExtraction import OrderMaskExtraction 
@@ -21,14 +22,17 @@ print("")
 
 # Create a Master Bias Image
 
-raw_bias_paths = ['Data/RawData/CalibrationImages/Bias/bias_0001.fits',
-                  'Data/RawData/CalibrationImages/Bias/bias_0002.fits',
-                  'Data/RawData/CalibrationImages/Bias/bias_0003.fits',
-                  'Data/RawData/CalibrationImages/Bias/bias_0004.fits',
-                  'Data/RawData/CalibrationImages/Bias/bias_0005.fits',
-                  'Data/RawData/CalibrationImages/Bias/bias_0006.fits',
-                  'Data/RawData/CalibrationImages/Bias/bias_0007.fits',
-                  'Data/RawData/CalibrationImages/Bias/bias_0008.fits']
+raw_bias_paths = ['Data/RawData/CalibrationImages/Bias/20221028T164120_BBBBB_H_0000.fits',
+                  'Data/RawData/CalibrationImages/Bias/20221028T164326_BBBBB_H_0000.fits',
+                  'Data/RawData/CalibrationImages/Bias/20221028T164524_BBBBB_H_0000.fits',
+                  'Data/RawData/CalibrationImages/Bias/20221028T164710_BBBBB_H_0000.fits',
+                  'Data/RawData/CalibrationImages/Bias/20221028T164854_BBBBB_H_0000.fits',
+                  'Data/RawData/CalibrationImages/Bias/20221028T165043_BBBBB_H_0000.fits',
+                  'Data/RawData/CalibrationImages/Bias/20221028T165250_BBBBB_H_0000.fits',
+                  'Data/RawData/CalibrationImages/Bias/20221028T165442_BBBBB_H_0000.fits',
+                  'Data/RawData/CalibrationImages/Bias/20221028T165635_BBBBB_H_0000.fits',
+                  'Data/RawData/CalibrationImages/Bias/20221028T165813_BBBBB_H_0000.fits',
+                  'Data/RawData/CalibrationImages/Bias/20221028T165938_BBBBB_H_0000.fits']
 
 masterBias = MasterBias(db, BiasImages=raw_bias_paths)
 masterBias.run("masterBias.fits")
@@ -38,26 +42,31 @@ print("")
 
 # Create a Master Flat Image
 
-raw_flat_paths = ['Data/RawData/CalibrationImages/Flat/flat_0001.fits',
-                  'Data/RawData/CalibrationImages/Flat/flat_0002.fits',
-                  'Data/RawData/CalibrationImages/Flat/flat_0003.fits',
-                  'Data/RawData/CalibrationImages/Flat/flat_0004.fits',
-                  'Data/RawData/CalibrationImages/Flat/flat_0005.fits']
-master_bias_path = "Data/ProcessedData/MasterBias/masterBias.fits"
+raw_flat_paths = ['Data/RawData/CalibrationImages/Flat/20221028T172658_FFFFF_H_0004.fits',
+                  'Data/RawData/CalibrationImages/Flat/20221028T173510_FFFFF_H_0004.fits',
+                  'Data/RawData/CalibrationImages/Flat/20221028T174304_FFFFF_H_0004.fits',
+                  'Data/RawData/CalibrationImages/Flat/20221028T175108_FFFFF_H_0004.fits',
+                  'Data/RawData/CalibrationImages/Flat/20221028T175908_FFFFF_H_0004.fits',
+                  'Data/RawData/CalibrationImages/Flat/20221028T180713_FFFFF_H_0004.fits']
 
+master_bias_path = "Data/ProcessedData/MasterBias/masterBias.fits"
 masterFlat = MasterFlat(db, FlatImages=raw_flat_paths, BiasImages=master_bias_path)
 masterFlat.run("masterFlat.fits")
 
 
 
-# Correct a science image for the bias. We call the output a "calibrated science image". 
+# Correct science images for the bias. We call the output a "calibrated science image". 
 
-raw_science_path = "Data/RawData/ScienceFrames/science_0001.fits"
+raw_science_paths = ["Data/RawData/ScienceFrames/20221027T175106_TSSSS_H_0900.fits", 
+                     "Data/RawData/ScienceFrames/20221027T175724_TSSSS_H_0900.fits"]
 master_bias_path = "Data/ProcessedData/MasterBias/masterBias.fits"
 
-calibration = CalibratedScienceFrames(db, ScienceImages=raw_science_path,
-                                          BiasImages=master_bias_path)
-calibration.run("biasCorrectedScience.fits")
+for raw_science_path in raw_science_paths:
+    calibration = CalibratedScienceFrames(db, ScienceImages=raw_science_path, BiasImages=master_bias_path)
+    basename, extension = os.path.splitext(os.path.basename(raw_science_path))
+    outputPath = basename + "_BC" + extension
+    calibration.run(outputPath)
+
 print(" ")
 
 
@@ -71,13 +80,17 @@ print(" ")
 
 
 
-# Using the pixel mask, extract the orders of a science image
+# Using the pixel mask, extract the orders of a (bias corrected) science image
 
-science_image_path = "Data/ProcessedData/CalibratedScience/biasCorrectedScience.fits"
+science_image_paths = ["Data/ProcessedData/CalibratedScience/20221027T175106_TSSSS_H_0900_BC.fits", 
+                       "Data/ProcessedData/CalibratedScience/20221027T175724_TSSSS_H_0900_BC.fits"]
+
 order_mask_path    = "Data/ProcessedData/ExtractedOrders/orderMask.fits"
-orderExtractor = OrderExtraction(db, debug=1, ExtractedOrders=order_mask_path,
-                                              ScienceImages=science_image_path)
-orderExtractor.run("extractedOrdersScience.fits")
+for science_image_path in science_image_paths:
+    orderExtractor = OrderExtraction(db, debug=1, ExtractedOrders=order_mask_path, ScienceImages=science_image_path)
+    basename, extension = os.path.splitext(os.path.basename(science_image_path))
+    outputPath = basename[:-3] + "_EO" + extension
+    orderExtractor.run(outputPath)
 print("")
 
 
@@ -85,18 +98,27 @@ print("")
 # Extract a 1D spectrum for each order, not yet wavelength calibrated but corrected for
 # the instrumental response curve. We call this an "optimal science extraction".
 
-extracted_science_path = "Data/ProcessedData/ExtractedOrders/extractedOrdersScience.fits"
+extracted_science_paths = ["Data/ProcessedData/ExtractedOrders/20221027T175106_TSSSS_H_0900_EO.fits", 
+                           "Data/ProcessedData/ExtractedOrders/20221027T175724_TSSSS_H_0900_EO.fits"]
 extracted_flat_path = "Data/ProcessedData/ExtractedOrders/orderMask.fits"
-optimalScience = OptimalExtraction(db, debug=1, ExtractedOrders=[extracted_science_path, extracted_flat_path])
-optimalScience.run("optimal_extracted_science_flux.fits")
+for extracted_science_path in extracted_science_paths:
+    optimalScience = OptimalExtraction(db, debug=1, ExtractedOrders=[extracted_science_path, extracted_flat_path])
+    basename, extension = os.path.splitext(os.path.basename(extracted_science_path))
+    outputPath = basename[:-3] + "_OP" + extension
+    optimalScience.run(outputPath)
 print("")
 
 
 # Do the wavelength calibration
 
-optimal_extracted_science_path = "Data/ProcessedData/OptimalExtraction/optimal_extracted_science_flux.fits"
-wavelength_calibration = WavelengthCalibration(db, debug=3, OptimalExtracted=optimal_extracted_science_path)
-wavelength_calibration.run("wavelength_calibrated_science.fits")
+optimal_extracted_science_paths = ["Data/ProcessedData/OptimalExtraction/20221027T175106_TSSSS_H_0900_OP.fits", 
+                                   "Data/ProcessedData/OptimalExtraction/20221027T175724_TSSSS_H_0900_OP.fits"]
+for optimal_extracted_science_path in optimal_extracted_science_paths:
+    wavelength_calibration = WavelengthCalibration(db, debug=1, OptimalExtracted=optimal_extracted_science_path)
+    basename, extension = os.path.splitext(os.path.basename(optimal_extracted_science_path))
+    outputPath = basename[:-3] + "_WC" + extension
+    wavelength_calibration.run(outputPath)
+print("")
 
 
 
